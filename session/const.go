f2d277f97dba933e4056f703fa68279bae10e15a
carl <carl@gmail.com>
2021-03-10 01:09:07 +0700

    first

---
+++ session/const.go
@@ -0,0 +1,19 @@
+package session
+
+import "time"
+
+const defaultSessionKeyName = "dj"
+const defaultDomain = ""
+const defaultExpires = 2 * time.Hour
+const defaultDemoExpires = 5 * time.Minute // 试玩登录默认过期时间
+
+//todo 开发阶段设置为一个星期,后续改回来
+const defaultGCLifetime = 24 * 7 * time.Hour
+
+//const defaultGCLifetime = 60 * time.Second
+const defaultSecure = false
+const defaultSessionIDInURLQuery = false
+const defaultSessionIDInHTTPHeader = false
+const defaultCookieLen uint32 = 32
+
+const expirationAttributeKey = "_sid_"
