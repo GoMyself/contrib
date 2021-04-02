021c58cbb0097be1f37d122046fecf656fa71200
carl <carl@gmail.com>
2021-04-02 14:48:59 +0700

    added JsonUnmarshal

--- helper/json.go
+++ helper/json.go
@@ -2,10 +2,16 @@ package helper
 
 import (
 	"github.com/wI2L/jettison"
+	jsoniter "github.com/json-iterator/go"
 )
 
+var cjson = jsoniter.ConfigCompatibleWithStandardLibrary
 
 func JsonMarshal(v interface{}) ([]byte, error) {
 
 	return jettison.Marshal(v)
+}
+
+func JsonUnmarshal(data []byte, v interface{}) error {
+	return cjson.Unmarshal(data, v)
 }
