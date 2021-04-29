a89c7d19cf14dac9c7051f6edd086dd10aca00f7
sar <sar@ym.com>
2021-04-29 13:00:55 +0700

    +越南语真实姓名

--- helper/strings_test.go
+++ helper/strings_test.go
@@ -7,5 +7,6 @@ func TestHide(t *testing.T) {
 	t.Log(Hide("438953453948537",TypeBankCardNumber))
 	t.Log(Hide("1PyMi4EYzGZKoxK7DozMMuoQ91EdrMMkBP",TypeVirtualCurrencyAddress))
 	t.Log(Hide("邓陈芳",TypeRealName))
+	t.Log(Hide("Nguyen Thanh Huyen",TypeRealName))
 	t.Log(Hide("148213318@qq.com",TypeEmail))
 }
