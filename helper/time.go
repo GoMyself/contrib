8a28a987179bb1fa3329a8dfd9b15c5dc168ec7e
sarr <sarr@ym.com>
2021-06-24 17:49:39 +0700

    返回time.Time

--- helper/time.go
+++ helper/time.go
@@ -16,67 +16,67 @@ func CtypeMonth(s string, loc *time.Location) (int64, error) {
 
 // 通过时间戳，获取一天的开始时间
 // 默认为当天的 00：00：00 时间戳
-func DayTST(timestamp int64, loc *time.Location) int64 {
+func DayTST(timestamp int64, loc *time.Location) time.Time {
 
 	t := time.Now().In(loc)
 	if timestamp > 0 {
 		t = time.Unix(timestamp, 0).In(loc)
 	}
 
-	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, loc).Unix()
+	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, loc)
 }
 
 // 通过时间戳，获取一天的结束时间
 // 默认为当天的 23：59：59 时间戳
-func DayTET(timestamp int64, loc *time.Location) int64 {
+func DayTET(timestamp int64, loc *time.Location) time.Time {
 
 	t := time.Now().In(loc)
 	if timestamp > 0 {
 		t = time.Unix(timestamp, 0).In(loc)
 	}
 
-	return time.Date(t.Year(), t.Month(), t.Day(), 23, 59, 59, 999999999, loc).Unix()
+	return time.Date(t.Year(), t.Month(), t.Day(), 23, 59, 59, 999999999, loc)
 }
 
 // 通过日期字符串，获取一天的开始时间
 // 默认为当天的 00：00：00 时间戳
-func DaySST(date string, loc *time.Location) int64 {
+func DaySST(date string, loc *time.Location) time.Time {
 
 	t := time.Now().In(loc)
 	if date != "" {
 		t = StrToTime(date, loc)
 	}
 
-	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, loc).Unix()
+	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, loc)
 }
 
 // 通过时间戳，获取一天的结束时间
 // 默认为当天的 23：59：59 时间戳
-func DaySET(date string, loc *time.Location) int64 {
+func DaySET(date string, loc *time.Location) time.Time {
 
 	t := time.Now().In(loc)
 	if date != "" {
 		t = StrToTime(date, loc)
 	}
 
-	return time.Date(t.Year(), t.Month(), t.Day(), 23, 59, 59, 999999999, loc).Unix()
+	return time.Date(t.Year(), t.Month(), t.Day(), 23, 59, 59, 999999999, loc)
 }
 
 // 通过时间戳，获取一月的开始时间
 // 默认为当前月的第一天 00：00：00 时间戳
-func MonthTST(timestamp int64, loc *time.Location) int64 {
+func MonthTST(timestamp int64, loc *time.Location) time.Time {
 
 	t := time.Now().In(loc)
 	if timestamp > 0 {
 		t = time.Unix(timestamp, 0).In(loc)
 	}
 
-	return time.Date(t.Year(), t.Month(), 1, 0, 0, 0, 0, loc).Unix()
+	return time.Date(t.Year(), t.Month(), 1, 0, 0, 0, 0, loc)
 }
 
 // 通过时间戳，获取一月的结束时间
 // 默认为当前月的最后一天 23：59：59 时间戳
-func MonthTET(timestamp int64, loc *time.Location) int64 {
+func MonthTET(timestamp int64, loc *time.Location) time.Time {
 
 	t := time.Now().In(loc)
 	if timestamp > 0 {
@@ -84,24 +84,24 @@ func MonthTET(timestamp int64, loc *time.Location) int64 {
 	}
 
 	t = time.Date(t.Year(), t.Month(), 1, 23, 59, 59, 999999999, loc)
-	return t.AddDate(0, 1, -1).Unix()
+	return t.AddDate(0, 1, -1)
 }
 
 // 通过日期字符串，获取一月的开始时间
 // 默认为当前月的第一天 00：00：00 时间戳
-func MonthSST(date string, loc *time.Location) int64 {
+func MonthSST(date string, loc *time.Location) time.Time {
 
 	t := time.Now().In(loc)
 	if date != "" {
 		t = StrToTime(date, loc)
 	}
 
-	return time.Date(t.Year(), t.Month(), 1, 0, 0, 0, 0, loc).Unix()
+	return time.Date(t.Year(), t.Month(), 1, 0, 0, 0, 0, loc)
 }
 
 // 通过日期字符串，获取一月的结束时间
 // 默认为当前月的最后一天 23：59：59 时间戳
-func MonthSET(date string, loc *time.Location) int64 {
+func MonthSET(date string, loc *time.Location) time.Time {
 
 	t := time.Now().In(loc)
 	if date != "" {
@@ -109,5 +109,5 @@ func MonthSET(date string, loc *time.Location) int64 {
 	}
 
 	t = time.Date(t.Year(), t.Month(), 1, 23, 59, 59, 999999999, loc)
-	return t.AddDate(0, 1, -1).Unix()
+	return t.AddDate(0, 1, -1)
 }
