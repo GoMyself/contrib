5a9659e0f3ecb0512441069855f5cdc7de55d2e1
carl <carl@gmail.com>
2021-12-29 15:27:16 +0400

    added new log

---
+++ tdlog/main.go
@@ -0,0 +1,115 @@
+package tdlog
+
+import (
+	"crypto/tls"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/valyala/fasthttp"
+)
+
+var fc = &fasthttp.Client{
+	Name:                     "TDlog",
+	NoDefaultUserAgentHeader: true,
+	TLSConfig:                &tls.Config{InsecureSkipVerify: true},
+	MaxConnsPerHost:          2000,
+	MaxIdleConnDuration:      5 * time.Second,
+	MaxConnDuration:          5 * time.Second,
+	ReadTimeout:              5 * time.Second,
+	WriteTimeout:             5 * time.Second,
+	MaxConnWaitTimeout:       5 * time.Second,
+}
+
+var (
+	timeout       time.Duration
+	internalUrl   string
+	internalToken string
+)
+
+func httpDoTimeout(requestBody []byte, method string, requestURI string, headers map[string]string) ([]byte, int, error) {
+
+	req := fasthttp.AcquireRequest()
+	resp := fasthttp.AcquireResponse()
+
+	defer func() {
+		fasthttp.ReleaseResponse(resp)
+		fasthttp.ReleaseRequest(req)
+	}()
+
+	req.SetRequestURI(requestURI)
+	req.Header.SetMethod(method)
+
+	switch method {
+	case "POST":
+		req.SetBody(requestBody)
+	}
+
+	if headers != nil {
+		for k, v := range headers {
+			req.Header.Set(k, v)
+		}
+	}
+
+	// time.Second * 30
+	err := fc.DoTimeout(req, resp, timeout)
+
+	return resp.Body(), resp.StatusCode(), err
+}
+
+func write(fields map[string]string, flags string) error {
+
+	var b strings.Builder
+
+	b.WriteString("INSERT INTO tb_name (ts, filename, content, fn, flags, id, project) VALUES(")
+	b.WriteString(fmt.Sprintf("%d,", time.Now().UnixMilli()))
+	b.WriteByte('"')
+	b.WriteString(fields["filename"])
+	b.WriteByte('"')
+	b.WriteByte(',')
+	b.WriteByte('"')
+	b.WriteString(fields["content"])
+	b.WriteByte('"')
+	b.WriteByte(',')
+	b.WriteByte('"')
+	b.WriteString(fields["fn"])
+	b.WriteByte('"')
+	b.WriteByte(',')
+	b.WriteByte('"')
+	b.WriteString(fields["flags"])
+	b.WriteByte('"')
+	b.WriteByte(',')
+	b.WriteByte('"')
+	b.WriteString(fields["id"])
+	b.WriteByte('"')
+	b.WriteByte(',')
+	b.WriteByte('"')
+	b.WriteString(fields["project"])
+	b.WriteByte('"')
+
+	fmt.Println("b = ", b.String())
+	headers := map[string]string{
+		"Authorization": "Basic " + internalToken,
+	}
+	httpDoTimeout([]byte(b.String()), "POST", "/rest/sql", headers)
+
+	return nil
+}
+
+func New(url, token string) {
+	internalUrl = url
+	internalToken = token
+	timeout = 5 * time.Second
+}
+
+func Info(fields map[string]string) error {
+	return write(fields, "info")
+}
+
+func Warn(fields map[string]string) error {
+	return write(fields, "warn")
+}
+
+func Error(fields map[string]string) error {
+	return write(fields, "error")
+}
