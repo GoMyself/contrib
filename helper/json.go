77d3d216bfe41e97c0a82c2792f8ed969df1469d
brandon <brandon@ym.com>
2022-05-04 15:32:26 +0700

    log

--- helper/json.go
+++ helper/json.go
@@ -1,15 +1,16 @@
 package helper
 
 import (
+	"fmt"
 	"github.com/goccy/go-json"
 )
 
-
 func JsonMarshal(v interface{}) ([]byte, error) {
-
+	fmt.Println("JsonMarshal", v)
 	return json.Marshal(v)
 }
 
 func JsonUnmarshal(data []byte, v interface{}) error {
+	fmt.Println("JsonUnmarshal", data)
 	return json.Unmarshal(data, v)
 }
