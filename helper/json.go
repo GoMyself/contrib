b2f845d97dd4c1ec107615a1721ac7805266f1d7
brandon <brandon@ym.com>
2022-05-04 16:58:24 +0700

    fix

--- helper/json.go
+++ helper/json.go
@@ -1,18 +1,15 @@
 package helper
 
 import (
-	"fmt"
 	jsoniter "github.com/json-iterator/go"
 )
 
 var json = jsoniter.ConfigCompatibleWithStandardLibrary
 
 func JsonMarshal(v interface{}) ([]byte, error) {
-	fmt.Println("JsonMarshal", v)
 	return json.Marshal(v)
 }
 
 func JsonUnmarshal(data []byte, v interface{}) error {
-	fmt.Println("JsonUnmarshal", data)
 	return json.Unmarshal(data, v)
 }
