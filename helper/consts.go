2248690842ea13e56cd0afaa55eed7d5961c5a1b
brandon <brandon@gmail.com>
2022-07-24 17:16:29 +0700

    +1285/1286/1287/1288

--- helper/consts.go
+++ helper/consts.go
@@ -295,4 +295,8 @@ const (
 	PlatformTransferInErr          = "1282" //场馆转入失败
 	PlatformTransferOutErr         = "1283" //场馆转出失败
 	PlatformTransferCheckErr       = "1284" //场馆转账查单失败
+	PlatformTransferInSuccess      = "1285" //场馆转入成功
+	PlatformTransferInFailed       = "1286" //场馆转入失败
+	PlatformTransferOutSuccess     = "1287" //场馆转出成功
+	PlatformTransferOutFailed      = "1288" //场馆转出失败
 )
